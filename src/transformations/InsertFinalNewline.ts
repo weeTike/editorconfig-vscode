5915aeab9dda1aabb27bb1a27da90c73186e0b0b
febLey <mail@febley.me>
2017-08-14 01:37:34 +0200

    Reworked InsertFinalNewline class

--- src/transformations/InsertFinalNewline.ts
+++ src/transformations/InsertFinalNewline.ts
@@ -3,6 +3,7 @@ import * as editorconfig from 'editorconfig';
 import {
 	TextDocument,
 	Position,
+	Range,
 	TextEdit
 } from 'vscode';
 
@@ -23,22 +24,59 @@ export default class InsertFinalNewline extends PreSaveTransformation {
 		const lineCount = doc.lineCount;
 		const lastLine = doc.lineAt(lineCount - 1);
 
-		if (!editorconfigProperties.insert_final_newline
-			|| lineCount === 0
-			|| lastLine.isEmptyOrWhitespace) {
+		if (lineCount === 0) {
 			return { edits: [] };
 		}
 
-		const position = new Position(
-			lastLine.lineNumber,
-			lastLine.text.length
-		);
+		if (editorconfigProperties.insert_final_newline) {
+			if (lastLine.isEmptyOrWhitespace) {
+				return { edits: [] };
+			} else {
+				const position = new Position(
+					lastLine.lineNumber,
+					lastLine.text.length
+				);
 
-		const eol = get(editorconfigProperties, 'end_of_line', 'lf').toUpperCase();
+				const eol = get(editorconfigProperties, 'end_of_line', 'lf').toUpperCase();
 
-		return {
-			edits: [ TextEdit.insert(position, this.lineEndings[eol]) ],
-			message: `insertFinalNewline(${eol})`
-		};
+				return {
+					edits: [ TextEdit.insert(position, this.lineEndings[eol]) ],
+					message: `insertFinalNewline(${eol})`
+				};
+			}
+		} else {
+			if (lastLine.isEmptyOrWhitespace) {
+				let realLastLine = lastLine;
+				let realLastLineDetected = false;
+				let lineCounter = 1;
+
+				while (!realLastLineDetected) {
+					if (!realLastLine.isEmptyOrWhitespace) {
+						realLastLineDetected = true;
+					} else {
+						lineCounter++;
+						realLastLine = doc.lineAt(lineCount - lineCounter);
+					}
+				}
+
+				const rangeStart = realLastLine.range;
+				const rangeEnd = lastLine.range;
+
+				const positionStart = rangeStart[Object.keys(rangeStart)[1]];
+				const positionEnd = rangeEnd[Object.keys(rangeEnd)[1]];
+
+				const range = new Range(
+					positionStart,
+					positionEnd
+				);
+
+				return {
+					edits: [ TextEdit.delete(range) ],
+					message: `deleteRange(${range})`
+				};
+			} else {
+				return { edits: [] };
+			}
+		}
 	}
 }
