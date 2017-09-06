23102961fc9a29fe7b797b3841dbc2e53f139b9a
Jed Mao <jedmao@outlook.com>
2017-09-06 13:52:24 -0700

    Revert "Reworked InsertFinalNewline class"
    This reverts commit 5915aeab9dda1aabb27bb1a27da90c73186e0b0b.

--- src/transformations/InsertFinalNewline.ts
+++ src/transformations/InsertFinalNewline.ts
@@ -3,7 +3,6 @@ import * as editorconfig from 'editorconfig';
 import {
 	TextDocument,
 	Position,
-	Range,
 	TextEdit
 } from 'vscode';
 
@@ -24,59 +23,22 @@ export default class InsertFinalNewline extends PreSaveTransformation {
 		const lineCount = doc.lineCount;
 		const lastLine = doc.lineAt(lineCount - 1);
 
-		if (lineCount === 0) {
+		if (!editorconfigProperties.insert_final_newline
+			|| lineCount === 0
+			|| lastLine.isEmptyOrWhitespace) {
 			return { edits: [] };
 		}
 
-		if (editorconfigProperties.insert_final_newline) {
-			if (lastLine.isEmptyOrWhitespace) {
-				return { edits: [] };
-			} else {
-				const position = new Position(
-					lastLine.lineNumber,
-					lastLine.text.length
-				);
+		const position = new Position(
+			lastLine.lineNumber,
+			lastLine.text.length
+		);
 
-				const eol = get(editorconfigProperties, 'end_of_line', 'lf').toUpperCase();
+		const eol = get(editorconfigProperties, 'end_of_line', 'lf').toUpperCase();
 
-				return {
-					edits: [ TextEdit.insert(position, this.lineEndings[eol]) ],
-					message: `insertFinalNewline(${eol})`
-				};
-			}
-		} else {
-			if (lastLine.isEmptyOrWhitespace) {
-				let realLastLine = lastLine;
-				let realLastLineDetected = false;
-				let lineCounter = 1;
-
-				while (!realLastLineDetected) {
-					if (!realLastLine.isEmptyOrWhitespace) {
-						realLastLineDetected = true;
-					} else {
-						lineCounter++;
-						realLastLine = doc.lineAt(lineCount - lineCounter);
-					}
-				}
-
-				const rangeStart = realLastLine.range;
-				const rangeEnd = lastLine.range;
-
-				const positionStart = rangeStart[Object.keys(rangeStart)[1]];
-				const positionEnd = rangeEnd[Object.keys(rangeEnd)[1]];
-
-				const range = new Range(
-					positionStart,
-					positionEnd
-				);
-
-				return {
-					edits: [ TextEdit.delete(range) ],
-					message: `deleteRange(${range})`
-				};
-			} else {
-				return { edits: [] };
-			}
-		}
+		return {
+			edits: [ TextEdit.insert(position, this.lineEndings[eol]) ],
+			message: `insertFinalNewline(${eol})`
+		};
 	}
 }
