8b9dac33bca1a1b40f597cdf7c06326950411f60
Jed Mao <jedmao@outlook.com>
2017-09-06 13:52:13 -0700

    Revert "Moved logic into seperate functions"
    This reverts commit e35a044256b42076376e6975684221f1177be8ee.

--- src/transformations/InsertFinalNewline.ts
+++ src/transformations/InsertFinalNewline.ts
@@ -17,61 +17,6 @@ export default class InsertFinalNewline extends PreSaveTransformation {
 		LF: '\n'
 	};
 
-	private deleteFinalNewlines = (doc) => {
-		const lineCount = doc.lineCount;
-		const lastLine = doc.lineAt(lineCount - 1);
-
-		let realLastLine = lastLine;
-		let realLastLineDetected = false;
-		let lineCounter = 1;
-
-		while (!realLastLineDetected) {
-			if (!realLastLine.isEmptyOrWhitespace) {
-				realLastLineDetected = true;
-			} else {
-				lineCounter++;
-				realLastLine = doc.lineAt(lineCount - lineCounter);
-			}
-		}
-
-		const rangeStart = realLastLine.range;
-		const rangeEnd = lastLine.range;
-
-		const positionStart = rangeStart[Object.keys(rangeStart)[1]];
-		const positionEnd = rangeEnd[Object.keys(rangeEnd)[1]];
-
-		const range = new Range(
-			positionStart,
-			positionEnd
-		);
-
-		return {
-			edits: [ TextEdit.delete(range) ],
-			message: `deleteRange(${range})`
-		};
-	}
-
-	private doNothing = () => {
-		return { edits: [] };
-	}
-
-	private insertFinalNewline = (editorconfigProperties, doc) => {
-		const lineCount = doc.lineCount;
-		const lastLine = doc.lineAt(lineCount - 1);
-
-		const position = new Position(
-			lastLine.lineNumber,
-			lastLine.text.length
-		);
-
-		const eol = get(editorconfigProperties, 'end_of_line', 'lf').toUpperCase();
-
-		return {
-			edits: [ TextEdit.insert(position, this.lineEndings[eol]) ],
-			message: `insertFinalNewline(${eol})`
-		};
-	}
-
 	transform(
 		editorconfigProperties: editorconfig.knownProps,
 		doc: TextDocument
@@ -80,20 +25,57 @@ export default class InsertFinalNewline extends PreSaveTransformation {
 		const lastLine = doc.lineAt(lineCount - 1);
 
 		if (lineCount === 0) {
-			return this.doNothing();
+			return { edits: [] };
 		}
 
 		if (editorconfigProperties.insert_final_newline) {
 			if (lastLine.isEmptyOrWhitespace) {
-				return this.doNothing();
+				return { edits: [] };
 			} else {
-				return this.insertFinalNewline(editorconfigProperties, doc);
+				const position = new Position(
+					lastLine.lineNumber,
+					lastLine.text.length
+				);
+
+				const eol = get(editorconfigProperties, 'end_of_line', 'lf').toUpperCase();
+
+				return {
+					edits: [ TextEdit.insert(position, this.lineEndings[eol]) ],
+					message: `insertFinalNewline(${eol})`
+				};
 			}
 		} else {
 			if (lastLine.isEmptyOrWhitespace) {
-				return this.deleteFinalNewlines(doc);
+				let realLastLine = lastLine;
+				let realLastLineDetected = false;
+				let lineCounter = 1;
+
+				while (!realLastLineDetected) {
+					if (!realLastLine.isEmptyOrWhitespace) {
+						realLastLineDetected = true;
+					} else {
+						lineCounter++;
+						realLastLine = doc.lineAt(lineCount - lineCounter);
+					}
+				}
+
+				const rangeStart = realLastLine.range;
+				const rangeEnd = lastLine.range;
+
+				const positionStart = rangeStart[Object.keys(rangeStart)[1]];
+				const positionEnd = rangeEnd[Object.keys(rangeEnd)[1]];
+
+				const range = new Range(
+					positionStart,
+					positionEnd
+				);
+
+				return {
+					edits: [ TextEdit.delete(range) ],
+					message: `deleteRange(${range})`
+				};
 			} else {
-				return this.doNothing();
+				return { edits: [] };
 			}
 		}
 	}
