6d7d02f28782f667fcaae476d4286d07631bef54
jedmao <jedmao@outlook.com>
2019-08-09 17:43:14 -0500

    run prettier + eslint

--- src/EditorConfigCompletionProvider.ts
+++ src/EditorConfigCompletionProvider.ts
@@ -4,209 +4,159 @@ import {
 	CompletionItem,
 	CompletionItemKind,
 	Position,
-	TextDocument
-} from 'vscode';
+	TextDocument,
+} from 'vscode'
 
 class Property {
-	name: string;
-	values: string[];
-	description: string;
+	public name: string
+	public values: string[]
+	public description: string
 
-	constructor(name: string, values: string[], description: string) {
-		this.name = name;
-		this.values = values;
-		this.description = description;
+	public constructor(name: string, values: string[], description: string) {
+		this.name = name
+		this.values = values
+		this.description = description
 	}
 }
 
 class EditorConfigCompletionProvider implements CompletionItemProvider {
-
 	private readonly properties: Property[] = [
 		new Property(
 			'root',
-			[
-				'true',
-				'false',
-				'unset'
-			],
+			['true', 'false', 'unset'],
 			[
 				'Special property that should be specified at the top of the file',
 				'outside of any sections. Set to true to stop .editorconfig files',
-				'search on current file.'
-			].join(' ')
+				'search on current file.',
+			].join(' '),
 		),
 		new Property(
 			'charset',
-			[
-				'utf-8',
-				'utf-8-bom',
-				'utf-16be',
-				'utf-16le',
-				'latin1',
-				'unset'
-			],
+			['utf-8', 'utf-8-bom', 'utf-16be', 'utf-16le', 'latin1', 'unset'],
 			[
 				'Set to latin1, utf-8, utf-8-bom, utf-16be or utf-16le to control',
-				'the character set. Use of utf-8-bom is discouraged.'
-			].join(' ')
+				'the character set. Use of utf-8-bom is discouraged.',
+			].join(' '),
 		),
 		new Property(
 			'end_of_line',
-			[
-				'lf',
-				'cr',
-				'crlf',
-				'unset'
-			],
-			'Set to lf, cr, or crlf to control how line breaks are represented.'
+			['lf', 'cr', 'crlf', 'unset'],
+			'Set to lf, cr, or crlf to control how line breaks are represented.',
 		),
 		new Property(
 			'indent_style',
-			[
-				'tab',
-				'space',
-				'unset'
-			],
-			'Set to tab or space to use hard tabs or soft tabs respectively.'
+			['tab', 'space', 'unset'],
+			'Set to tab or space to use hard tabs or soft tabs respectively.',
 		),
 		new Property(
 			'indent_size',
-			[
-				'1',
-				'2',
-				'3',
-				'4',
-				'5',
-				'6',
-				'7',
-				'8',
-				'unset'
-			],
+			['1', '2', '3', '4', '5', '6', '7', '8', 'unset'],
 			[
 				'A whole number defining the number of columns used for each',
 				'indentation level and the width of soft tabs (when supported).',
 				'When set to tab, the value of tab_width (if specified) will be',
-				'used.'
-			].join(' ')
+				'used.',
+			].join(' '),
 		),
 		new Property(
 			'insert_final_newline',
-			[
-				'true',
-				'false',
-				'unset'
-			],
+			['true', 'false', 'unset'],
 			[
 				'Set to true to ensure file ends with a newline when saving and',
-				'false to ensure it doesn\'t.'
-			].join(' ')
+				"false to ensure it doesn't.",
+			].join(' '),
 		),
 		new Property(
 			'tab_width',
-			[
-				'1',
-				'2',
-				'3',
-				'4',
-				'5',
-				'6',
-				'7',
-				'8',
-				'unset'
-			],
+			['1', '2', '3', '4', '5', '6', '7', '8', 'unset'],
 			[
 				'A whole number defining the number of columns used to represent a',
 				'tab character. This defaults to the value of indent_size and',
-				'doesn\'t usually need to be specified.'
-			].join(' ')
+				"doesn't usually need to be specified.",
+			].join(' '),
 		),
 		new Property(
 			'trim_trailing_whitespace',
-			[
-				'true',
-				'false',
-				'unset'
-			],
+			['true', 'false', 'unset'],
 			[
 				'Set to true to remove any whitespace characters preceding newline',
-				'characters and false to ensure it doesn\'t.'
-			].join(' ')
-		)
-	];
+				"characters and false to ensure it doesn't.",
+			].join(' '),
+		),
+	]
 
 	// =========================================================================
 	// PUBLIC INTERFACE
 	// =========================================================================
 	public provideCompletionItems(
 		document: TextDocument,
-		position: Position
+		position: Position,
 	): CompletionItem[] {
 		// get text where code completion was activated
 		// used to determine if autocompleting a key or a value
 		const textOfEntireLine = document.getText(
-			document.lineAt(position.line).range
-		);
+			document.lineAt(position.line).range,
+		)
 		const textOfLineUpToCursor = textOfEntireLine.substring(
-			0, position.character
-		);
+			0,
+			position.character,
+		)
 
 		// conditionally generate autocomplete for property names or values
 		if (this.hasPropertyKey(textOfLineUpToCursor)) {
-			return this.autoCompletePropertyValues(textOfLineUpToCursor);
+			return this.autoCompletePropertyValues(textOfLineUpToCursor)
 		} else {
-			return this.autoCompletePropertyNames(textOfEntireLine);
+			return this.autoCompletePropertyNames(textOfEntireLine)
 		}
 	}
 
-	public resolveCompletionItem(
-		item: CompletionItem
-	): CompletionItem {
+	public resolveCompletionItem(item: CompletionItem): CompletionItem {
 		// return the item itself because it already constains all the info
 		// necessary to display the details
-		return item;
+		return item
 	}
 
 	// =========================================================================
 	// AUTO COMPLETE
 	// =========================================================================
 	private autoCompletePropertyValues(
-		textOfLineUpToCursor: string
+		textOfLineUpToCursor: string,
 	): CompletionItem[] {
-		const propertyName = this.extractPropertyName(textOfLineUpToCursor);
-		const propertyValues = this.filterPropertyValues(propertyName);
-		return this.convertPropertyValuesToCompletionItems(propertyValues);
+		const propertyName = this.extractPropertyName(textOfLineUpToCursor)
+		const propertyValues = this.filterPropertyValues(propertyName)
+		return this.convertPropertyValuesToCompletionItems(propertyValues)
 	}
 
 	private autoCompletePropertyNames(
-		textOfEntireLine: string
+		textOfEntireLine: string,
 	): CompletionItem[] {
 		return this.convertPropertyNamesToCompletionItems(
 			this.properties,
-			textOfEntireLine
-		);
+			textOfEntireLine,
+		)
 	}
 
 	// =========================================================================
 	// CHECKS
 	// =========================================================================
 	private hasPropertyKey(lineText: string): boolean {
-		return this.hasEqualsSign(lineText);
+		return this.hasEqualsSign(lineText)
 	}
 
 	private hasEqualsSign(lineText: string): boolean {
-		return lineText.indexOf('=') >= 0;
+		return lineText.indexOf('=') >= 0
 	}
 
 	// =========================================================================
 	// PARSER
 	// =========================================================================
 	private extractPropertyName(lineText: string): string {
-		const lineTextParts = lineText.split('=');
+		const lineTextParts = lineText.split('=')
 		if (lineTextParts.length === 0) {
-			return '';
+			return ''
 		}
-		const propertyName = lineTextParts[0].trim().toLowerCase();
-		return propertyName;
+		const propertyName = lineTextParts[0].trim().toLowerCase()
+		return propertyName
 	}
 
 	// =========================================================================
@@ -215,16 +165,16 @@ class EditorConfigCompletionProvider implements CompletionItemProvider {
 	private filterPropertyValues(propertyName: string): string[] {
 		// filter
 		const matchingProperty = this.properties.find(
-			property => property.name === propertyName
-		);
+			property => property.name === propertyName,
+		)
 
 		// if not found anything, there are no values to display
 		if (matchingProperty === undefined) {
-			return [];
+			return []
 		}
 
 		// return values of the property
-		return matchingProperty.values;
+		return matchingProperty.values
 	}
 
 	// =========================================================================
@@ -232,53 +182,50 @@ class EditorConfigCompletionProvider implements CompletionItemProvider {
 	// =========================================================================
 	private convertPropertyNamesToCompletionItems(
 		properties: Property[],
-		textOfEntireLine: string
+		textOfEntireLine: string,
 	): CompletionItem[] {
-
 		const triggerSuggestCommand: Command = {
 			command: 'editorconfig._triggerSuggestAfterDelay',
 			arguments: [],
-			title: ''
-		};
+			title: '',
+		}
 
 		return properties.map(property => {
 			// basic info
 			const completionItem = new CompletionItem(
 				property.name,
-				CompletionItemKind.Property
-			);
-			completionItem.documentation = property.description;
+				CompletionItemKind.Property,
+			)
+			completionItem.documentation = property.description
 
 			// append equals sign if line does not have one
 			// also automatically displays IntelliSense for property values
 			if (!this.hasEqualsSign(textOfEntireLine)) {
-				completionItem.insertText = property.name + ' = ';
-				completionItem.command = triggerSuggestCommand;
+				completionItem.insertText = property.name + ' = '
+				completionItem.command = triggerSuggestCommand
 			}
 
-			return completionItem;
-		});
+			return completionItem
+		})
 	}
 
 	private convertPropertyValuesToCompletionItems(
-		values: string[]
+		values: string[],
 	): CompletionItem[] {
 		const valuesSortOrder = {
 			true: '1',
 			false: '2',
-			unset: '9'
-		};
-		return values.map(
-			value => {
-				// basic info
-				const completionItem = new CompletionItem(value, CompletionItemKind.Value);
+			unset: '9',
+		}
+		return values.map(value => {
+			// basic info
+			const completionItem = new CompletionItem(value, CompletionItemKind.Value)
 
-				// sort predefined values to specific order
-				completionItem.sortText = valuesSortOrder[value] || '3' + value;
-				return completionItem;
-			}
-		);
+			// sort predefined values to specific order
+			completionItem.sortText = valuesSortOrder[value] || '3' + value
+			return completionItem
+		})
 	}
 }
 
-export default EditorConfigCompletionProvider;
+export default EditorConfigCompletionProvider
--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,10 @@
-import * as editorconfig from 'editorconfig';
-import * as compact from 'lodash.compact';
-import * as get from 'lodash.get';
-import * as path from 'path';
-import {
-	window,
-	workspace,
-	TextDocument,
-	TextEditorOptions,
-} from 'vscode';
+import * as editorconfig from 'editorconfig'
+import * as compact from 'lodash.compact'
+import * as get from 'lodash.get'
+import * as path from 'path'
+import { window, workspace, TextDocument, TextEditorOptions } from 'vscode'
 
-import languageExtensionMap from './languageExtensionMap';
+import languageExtensionMap from './languageExtensionMap'
 
 /**
  * Resolves `TextEditorOptions` for a `TextDocument`, combining the editor's
@@ -21,20 +16,20 @@ export async function resolveTextEditorOptions(
 		defaults = pickWorkspaceDefaults(),
 		onBeforeResolve,
 		onEmptyConfig,
-	}:	{
-		defaults?: TextEditorOptions;
-		onBeforeResolve?: (relativePath: string) => void;
-		onEmptyConfig?: (relativePath?: string) => void;
+	}: {
+		defaults?: TextEditorOptions
+		onBeforeResolve?: (relativePath: string) => void
+		onEmptyConfig?: (relativePath?: string) => void
 	} = {},
 ) {
-	const editorconfigSettings = await resolveCoreConfig(doc, { onBeforeResolve });
+	const editorconfigSettings = await resolveCoreConfig(doc, { onBeforeResolve })
 	if (editorconfigSettings) {
-		return fromEditorConfig(editorconfigSettings, defaults);
+		return fromEditorConfig(editorconfigSettings, defaults)
 	}
 	if (onEmptyConfig) {
-		onEmptyConfig(resolveFile(doc).relativePath);
+		onEmptyConfig(resolveFile(doc).relativePath)
 	}
-	return {};
+	return {}
 }
 
 /**
@@ -45,23 +40,23 @@ export async function applyTextEditorOptions(
 	{
 		onNoActiveTextEditor,
 		onSuccess,
-	}:	{
-		onNoActiveTextEditor?: () => void;
-		onSuccess?: (newOptions: TextEditorOptions) => void;
+	}: {
+		onNoActiveTextEditor?: () => void
+		onSuccess?: (newOptions: TextEditorOptions) => void
 	} = {},
 ) {
-	const editor = window.activeTextEditor;
+	const editor = window.activeTextEditor
 	if (!editor) {
 		if (onNoActiveTextEditor) {
-			onNoActiveTextEditor();
+			onNoActiveTextEditor()
 		}
-		return;
+		return
 	}
 
-	editor.options = newOptions;
+	editor.options = newOptions
 
 	if (onSuccess) {
-		onSuccess(newOptions);
+		onSuccess(newOptions)
 	}
 }
 
@@ -73,24 +68,26 @@ export function pickWorkspaceDefaults(): {
 	 * The number of spaces a tab is equal to. When `editor.detectIndentation`
 	 * is on, this property value will be `undefined`.
 	 */
-	tabSize?: number;
+	tabSize?: number
 	/**
 	 * Insert spaces when pressing `Tab`. When `editor.detectIndentation` is on,
 	 * this property value will be `undefined`.
 	 */
-	insertSpaces?: boolean;
+	insertSpaces?: boolean
 } {
-	const workspaceConfig = workspace.getConfiguration('editor', null);
-	const detectIndentation = workspaceConfig.get<boolean>('detectIndentation');
+	const workspaceConfig = workspace.getConfiguration('editor', null)
+	const detectIndentation = workspaceConfig.get<boolean>('detectIndentation')
 
-	return (detectIndentation) ? {} : {
-		tabSize: workspaceConfig.get<number>('tabSize'),
-		insertSpaces: workspaceConfig.get<boolean>('insertSpaces')
-	};
+	return detectIndentation
+		? {}
+		: {
+				tabSize: workspaceConfig.get<number>('tabSize'),
+				insertSpaces: workspaceConfig.get<boolean>('insertSpaces'),
+		  }
 }
 
 export interface ResolvedCoreConfig extends editorconfig.KnownProps {
-	[rule: string]: string | number | boolean;
+	[rule: string]: string | number | boolean
 }
 
 /**
@@ -102,47 +99,49 @@ export async function resolveCoreConfig(
 	{
 		onBeforeResolve,
 	}: {
-		onBeforeResolve?: (relativePath: string) => void;
+		onBeforeResolve?: (relativePath: string) => void
 	} = {},
 ): Promise<ResolvedCoreConfig> {
-	const { fileName, relativePath } = resolveFile(doc);
+	const { fileName, relativePath } = resolveFile(doc)
 	if (!fileName) {
-		return {};
+		return {}
 	}
 	if (onBeforeResolve) {
-		onBeforeResolve(relativePath);
+		onBeforeResolve(relativePath)
 	}
-	const config = await editorconfig.parse(fileName);
+	const config = await editorconfig.parse(fileName)
 	if (config.indent_size === 'tab') {
-		config.indent_size = config.tab_width;
+		config.indent_size = config.tab_width
 	}
-	return config as ResolvedCoreConfig;
+	return config as ResolvedCoreConfig
 }
 
-export function resolveFile(doc: TextDocument): {
-	fileName?: string;
-	relativePath?: string;
+export function resolveFile(
+	doc: TextDocument,
+): {
+	fileName?: string
+	relativePath?: string
 } {
 	if (doc.languageId === 'Log') {
-		return {};
+		return {}
 	}
-	const fileName = getFileName();
+	const fileName = getFileName()
 	return {
 		fileName,
 		relativePath: workspace.asRelativePath(fileName, true),
-	};
+	}
 
 	function getFileName() {
 		if (!doc.isUntitled) {
-			return doc.fileName;
+			return doc.fileName
 		}
-		const ext = languageExtensionMap[doc.languageId] || doc.languageId;
+		const ext = languageExtensionMap[doc.languageId] || doc.languageId
 		return path.join(
 			...compact([
 				workspace.getWorkspaceFolder(doc.uri),
-				`${doc.fileName}.${ext}`
-			])
-		);
+				`${doc.fileName}.${ext}`,
+			]),
+		)
 	}
 }
 
@@ -154,51 +153,50 @@ export function fromEditorConfig(
 	defaults: TextEditorOptions = pickWorkspaceDefaults(),
 ): TextEditorOptions {
 	const resolved: TextEditorOptions = {
-		tabSize: (config.indent_style === 'tab'
-			? get(config, 'tab_width', config.indent_size)
-			: get(config, 'indent_size', config.tab_width)
-		)
-	};
+		tabSize:
+			config.indent_style === 'tab'
+				? get(config, 'tab_width', config.indent_size)
+				: get(config, 'indent_size', config.tab_width),
+	}
 	if (get(resolved, 'tabSize') === 'tab') {
-		resolved.tabSize = config.tab_width;
+		resolved.tabSize = config.tab_width
 	}
 	return {
-		...(config.indent_style === 'tab'
-			|| config.indent_size === 'tab'
-			|| config.indent_style === 'space'
-		) ? {
-			insertSpaces: config.indent_style === 'space'
-		} : {},
-		tabSize: resolved.tabSize >= 0
-			? resolved.tabSize
-			: defaults.tabSize
-	};
+		...(config.indent_style === 'tab' ||
+		config.indent_size === 'tab' ||
+		config.indent_style === 'space'
+			? {
+					insertSpaces: config.indent_style === 'space',
+			  }
+			: {}),
+		tabSize: resolved.tabSize >= 0 ? resolved.tabSize : defaults.tabSize,
+	}
 }
 
 /**
  * Convert vscode editor options to .editorconfig values
  */
 export function toEditorConfig(options: TextEditorOptions) {
-	const result: editorconfig.KnownProps = {};
+	const result: editorconfig.KnownProps = {}
 
 	switch (options.insertSpaces) {
 		case true:
-			result.indent_style = 'space';
-			result.indent_size = resolveTabSize(options.tabSize);
-			break;
+			result.indent_style = 'space'
+			result.indent_size = resolveTabSize(options.tabSize)
+			break
 		case false:
 		case 'auto':
-			result.indent_style = 'tab';
-			result.tab_width = resolveTabSize(options.tabSize);
-			break;
+			result.indent_style = 'tab'
+			result.tab_width = resolveTabSize(options.tabSize)
+			break
 	}
 
-	return result;
+	return result
 
 	/**
 	 * Convert vscode tabSize option into numeric value
 	 */
-	function resolveTabSize(tabSize: number|string) {
-		return (tabSize === 'auto') ? 4 : parseInt(String(tabSize), 10);
+	function resolveTabSize(tabSize: number | string) {
+		return tabSize === 'auto' ? 4 : parseInt(String(tabSize), 10)
 	}
 }
